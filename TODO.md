f3c77f5ef7c1655cd67d9b7ffefee9c5d630315b
Brady O'Leary <thebradyoleary@gmail.com>
2019-10-31 23:26:49 -0400

    Added Transform Color Panel

---
+++ TODO.md
@@ -0,0 +1,13 @@
+JS Fractal Explorer TODO File:
+
+Initial Release:
+    [ ] Implement Zooming and Panning using the Cursor (Wheel and click Events)
+        - May need to do a Low-resolution Render First before doing a second pass for Higher-resolution
+    [ ] Implement Fractal using Self-timeouts to allow for message passing (this is mainly a firefox problem)
+    [ ] Make Control Panel Hideable without using Inspect Element
+
+Future:
+    [ ] Refactor the code to make it more modular
+    [ ] Implement Other Coloring algorithms
+    [ ] Fully implement the Color Control Panel (Linear, Transform, etc...)
+    [ ] Add https://en.wikipedia.org/wiki/Tricorn_(mathematics)
--- main.js
+++ main.js
@@ -12,6 +12,14 @@ let red = 0;
 let green = 0;
 let blue = 0;
 
+// Color Transform
+window.tr_red_f = i => i;
+window.tr_red_i = false;
+window.tr_blue_f = i => i;
+window.tr_blue_i = false;
+window.tr_green_f = i => i;
+window.tr_green_i = false;
+
 const MAX_ITER = 1000;
 
 // function draw() {
@@ -58,10 +66,10 @@ const MAX_ITER = 1000;
 
 function init() {
     initDraw();
+    initTransformColorControls();
     initControls();
     initMandelbrot();
     drawMandelbrot(MAX_ITER);
-    showControlsFor("mandelbrot");
 }
 
 /**
@@ -73,9 +81,62 @@ function initDraw() {
     canvas = document.getElementById("canvas");
     canvas.setAttribute("width", width);//Set the Canvas size to the native screen resolution
     canvas.setAttribute("height", height);
+}
+
+function initLinearColorControls() {
     redslider = document.getElementById("red");
     greenslider = document.getElementById("green");
     blueslider = document.getElementById("blue");
+}
+
+function initTransformColorControls() {
+    redslider = document.getElementById("red");
+    greenslider = document.getElementById("green");
+    blueslider = document.getElementById("blue");
+    let trcp = document.querySelectorAll("#colorPanel [for=transform] div input");
+    trcp.forEach((item) => {
+        if (item.getAttribute("type") !== "range") {
+            item.oninput = (e) => {
+                let idsplit = e.target.id.split("_");
+                let color = idsplit[1];
+                let trName = idsplit[2];
+                //sin cos sqrt sqrd lin (i)
+                let invVal = window["tr_" + color + "_i"];
+
+                console.log("Changed:", color, trName, invVal);
+                
+                if (trName === "i") {
+                    window["tr_" + color + "_i"] = e.target.checked ? true : false;
+                } else if (trName === "sin") {
+                    window["tr_" + color + "_f"] = (i) => {
+                        let res = Math.sin(i);
+                        return res;
+                    }
+                } else if (trName === "cos") {
+                    window["tr_" + color + "_f"] = (i) => {
+                        let res = Math.cos(i);
+                        return res;
+                    }
+                } else if (trName === "sqrt") {
+                    window["tr_" + color + "_f"] = (i) => {
+                        let res = Math.sqrt(i);
+                        return res;
+                    }
+                } else if (trName === "sqrd") {
+                    window["tr_" + color + "_f"] = (i) => {
+                        let res = Math.pow(i, 2);
+                        return res;
+                    }
+                } else if (trName === "lin") {
+                    window["tr_" + color + "_f"] = (i) => {
+                        let res = i;
+                        return res;
+                    }
+                }
+                console.log("Function:", window["tr_" + color + "_f"]);
+            }
+        }
+    });
 }
 
 function initControls() {
@@ -111,6 +172,12 @@ function initMandelbrot() {
     let maxiter = document.querySelector("[for=mandelbrot] #maxiter");
     maxiter.setAttribute("max", MAX_ITER);
     maxiter.setAttribute("min", 0);
+    let zoom = document.querySelector("[for=mandelbrot] #zoom");
+    zoom.setAttribute("max", 10.0);
+    zoom.setAttribute("min", 1.0);
+}
+
+function enablePointSelect() {
 
 }
 
@@ -141,6 +208,7 @@ function cpDrawMandelbrot() {
     red = redslider.value;
     green = greenslider.value;
     blue = blueslider.value;
+    console.log(window.tr_red_f, window.tr_red_i);
     drawMandelbrot(maxiter);
 }
 
@@ -160,7 +228,7 @@ function drawMandelbrot(maxiter) {
             let x = 0.0;
             let y = 0.0;
             let iter = 0;
-            while (x*x + y*y <= 2*2 && iter < maxiter) {
+            while (x*x + y*y <= 4 && iter < maxiter) {
                 let xtemp = x*x - y*y + x0;
                 y = 2*x*y + y0;
                 x = xtemp;
@@ -172,7 +240,7 @@ function drawMandelbrot(maxiter) {
             // imgdata.data[arrpos + 1] = (iter/maxiter) * 255; // GREEN
             // imgdata.data[arrpos + 2] = (iter/maxiter) * 255; // BLUE
             // imgdata.data[arrpos + 3] = 255; // ALPHA
-            calcAndInsertColorsLinear(iter, maxiter, arrpos, imgdata);
+            calcAndInsertColorsTransform(iter, maxiter, arrpos, imgdata);
         }
     }
     ctx.putImageData(imgdata, 0, 0);
@@ -186,10 +254,35 @@ function calcAndInsertColors(iter, maxiter, arrpos, imgdata) {
     imgdata.data[arrpos + 3] = 255; // ALPHA
 }
 
-
 function calcAndInsertColorsLinear(iter, maxiter, arrpos, imgdata) {
     imgdata.data[arrpos] = (iter/maxiter) * red; // RED
     imgdata.data[arrpos + 1] = (iter/maxiter) * green; // GREEN
     imgdata.data[arrpos + 2] = (iter/maxiter) * blue; // BLUE
+    imgdata.data[arrpos + 3] = 255; // ALPHA
+}
+
+function calcAndInsertColorsSinCos(iter, maxiter, arrpos, imgdata) {
+    imgdata.data[arrpos] = Math.sin((iter/maxiter) * Math.PI) * red; // RED
+    imgdata.data[arrpos + 1] =  Math.cos((iter/maxiter) * Math.PI) * green; // GREEN
+    imgdata.data[arrpos + 2] = Math.sqrt(iter/maxiter) * blue; // BLUE
+    imgdata.data[arrpos + 3] = 255; // ALPHA
+}
+
+function calcAndInsertColorsTransform(iter, maxiter, arrpos, imgdata) {
+    if (tr_red_i) {
+        imgdata.data[arrpos] = (1.0 - tr_red_f(iter/maxiter)) * red; // RED
+    } else {
+        imgdata.data[arrpos] = tr_red_f(iter/maxiter) * red; // RED
+    }
+    if (tr_green_i) {
+        imgdata.data[arrpos + 1] = (1.0 - tr_green_f(iter/maxiter)) * green; // GREEN
+    } else {
+        imgdata.data[arrpos + 1] = tr_green_f(iter/maxiter) * green; // GREEN
+    }
+    if (tr_blue_i) {
+        imgdata.data[arrpos + 2] = (1.0 - tr_blue_f(iter/maxiter)) * blue; // BLUE
+    } else {
+        imgdata.data[arrpos + 2] = tr_blue_f(iter/maxiter) * blue; // BLUE
+    }
     imgdata.data[arrpos + 3] = 255; // ALPHA
 }
